05fd495cb6123dd0a8412c84227b020928aa1240
lixiang <lixiang9409@vip.qq.com>
2020-05-04 15:05:52 +0800

    改了sign的方法

--- sunshine-base/src/main/java/red/lixiang/tools/jdk/MapTools.java
+++ sunshine-base/src/main/java/red/lixiang/tools/jdk/MapTools.java
@@ -9,13 +9,14 @@ import java.util.stream.Collectors;
  * @date 2020/5/4
  **/
 public class MapTools {
-    public String preSign(Map<String,Object> paramMap){
+
+    public static String preSign(Map<String,String> paramMap){
         String[] keys = paramMap.keySet().toArray(new String[0]);
         Arrays.sort(keys);
         String preSign = Arrays.stream(keys)
                 .filter(x -> !x.equals("sign")  && (paramMap.get(x) != null))
                 .map(x ->{
-                    String s = String.valueOf(paramMap.get(x));
+                    String s = paramMap.get(x);
                     return x + "=" + s;
                 } )
                 .collect(Collectors.joining("&"));
