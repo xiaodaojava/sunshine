b38657244a850fbcb0114db9a4818a557b0a3044
lixiang <lixiang9409@vip.qq.com>
2020-05-23 17:01:27 +0800

    完成了文件的拆分和合并

--- sunshine-base/src/main/java/red/lixiang/tools/jdk/file/FileTools.java
+++ sunshine-base/src/main/java/red/lixiang/tools/jdk/file/FileTools.java
@@ -1,15 +1,20 @@
 package red.lixiang.tools.jdk.file;
 
+import red.lixiang.tools.base.exception.BusinessException;
+import red.lixiang.tools.jdk.ByteTools;
 import red.lixiang.tools.jdk.io.IOTools;
 import red.lixiang.tools.jdk.os.OSTools;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.*;
 import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static red.lixiang.tools.jdk.file.FilePart.FIFTY_MB;
 
 /**
  * @Author lixiang
@@ -26,6 +31,11 @@ public class FileTools {
         return fileName.substring(fileName.lastIndexOf("."));
     }
 
+    /**
+     * 从文件绝对路径中获取文件名
+     * @param path
+     * @return
+     */
     public static String getFileNameFromPath(String path){
         String separator = OSTools.fileSeparator();
         if(!path.contains(separator)){
@@ -33,6 +43,17 @@ public class FileTools {
         }else {
             int i = path.lastIndexOf(separator);
             path = path.substring(i+1);
+            return path;
+        }
+    }
+
+    public static String getDirFromPath(String path){
+        String separator = OSTools.fileSeparator();
+        if(!path.contains(separator)){
+            return ".";
+        }else {
+            int i = path.lastIndexOf(separator);
+            path = path.substring(0,i+1);
             return path;
         }
     }
@@ -91,7 +112,7 @@ public class FileTools {
     }
 
 
-    public static FilePart getFromFile(File file,Integer readSize){
+    public static FilePart partInfoFromFile(File file,Integer readSize){
         FilePart filePart = new FilePart();
         String fileName = file.getName();
         // 文件的总长度
@@ -133,11 +154,17 @@ public class FileTools {
         // 开始的位置
         long startIndex = (startPart-1)*readSize;
         filePart.setTotalLength(totalLength)
-                .setCurrentLength(readSize)
                 .setTotalPart(totalPart)
                 .setCurrentPart(startPart)
                 .setFileName(fileName);
-        byte[] bytes = new byte[readSize];
+
+        byte[] bytes;
+        if(startPart == totalPart){
+            bytes = new byte[(int) (totalLength-startIndex)];
+        }else {
+            bytes = new byte[readSize];
+        }
+        filePart.setCurrentLength(bytes.length);
         try (RandomAccessFile randomFile = new RandomAccessFile(file, "r")) {
             randomFile.seek(startIndex);
             randomFile.read(bytes);
@@ -148,10 +175,78 @@ public class FileTools {
         return filePart;
     }
 
+    public static void mergeFilePart(String workDir){
+        String descFile = workDir+"desc.hbb";
+        if(Files.notExists(Paths.get(descFile))){
+            throw new BusinessException("缺少描述文件:desc.hbb");
+        }
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(descFile));
+            FilePart filePart = ByteTools.byteToObject(bytes, FilePart.class);
+            String fileName = filePart.getFileName();
+            String filePath = workDir+fileName;
+//            File file = new File(workDir+fileName);
+//            RandomAccessFile randomFile = new RandomAccessFile(file,"rw");
 
+            SeekableByteChannel byteChannel = Files.newByteChannel(Paths.get(filePath), StandardOpenOption.CREATE,StandardOpenOption.WRITE,StandardOpenOption.APPEND);
+            //获取当前目录下的所有x.hbb文件,并排好序
+            long count = Files.list(Paths.get(workDir)).filter(x ->
+                    x.getFileName().toString().endsWith(".hbb")
+                            &&!(x.getFileName().toString().contains("desc")))
+                    .sorted().map(x -> {
+                        try {
+                            byte[] allBytes = Files.readAllBytes(x);
+                            FilePart part = ByteTools.byteToObject(allBytes, FilePart.class);
+                            byte[] contents = part.getContents();
+                            byteChannel.write(ByteBuffer.wrap(contents));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }).count();
+            System.out.println(count);
+            byteChannel.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        String suffixName = getSuffixNameNoDot("aa.jpg");
-        System.out.println(suffixName);
+        mergeFilePart("/Users/lixiang/Desktop/split/");
+    }
+
+    /**
+     *
+     * 把一个文件拆分成多个
+     *
+     * @param filePath
+     */
+    public static void splitFile(String filePath) {
+        //String filePath = "/Users/lixiang/Desktop/testPDF.pdf";
+        File file = new File(filePath);
+        String dirFromPath = FileTools.getDirFromPath(filePath);
+        String workDir = dirFromPath+"/split/";
+        FilePart filePart = partInfoFromFile(file, FIFTY_MB);
+        try {
+            Files.write(Paths.get(workDir+"desc.hbb"),ByteTools.objectToByte(filePart));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Integer totalPart = filePart.getTotalPart();
+        for (int i = 0; i < totalPart; i++) {
+            FilePart part = readToFilePart(file, i+1, FIFTY_MB);
+            if (part==null){
+                continue;
+            }
+            //先单独输出
+            byte[] bytes = ByteTools.objectToByte(part);
+            try {
+                Files.createDirectories(Paths.get(workDir));
+                Files.write(Paths.get(workDir + part.getCurrentPart() + ".hbb"),bytes,StandardOpenOption.CREATE );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
