4716cbb11d16069b81d32567482dc96e8eddd05c
lixiang <lixiang9409@vip.qq.com>
2020-06-16 20:19:01 +0800

    加了动态代理的DEMO

---
+++ sunshine-base/src/main/java/red/lixiang/tools/demo/proxy/dynamic/normal/GreetImpl.java
@@ -0,0 +1,15 @@
+package red.lixiang.tools.demo.proxy.dynamic.normal;
+
+import red.lixiang.tools.demo.proxy.dynamic.Greet;
+
+/**
+ * @author lixiang
+ * @date 2020/6/16
+ **/
+public class GreetImpl implements Greet {
+
+    @Override
+    public void cheer() {
+        System.out.println("加油, 为了美好的明天!");
+    }
+}
---
+++ sunshine-base/src/main/java/red/lixiang/tools/demo/proxy/dynamic/normal/GreetProxy.java
@@ -0,0 +1,28 @@
+package red.lixiang.tools.demo.proxy.dynamic.normal;
+
+import red.lixiang.tools.demo.proxy.dynamic.Greet;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author lixiang
+ * @date 2020/6/16
+ **/
+public class GreetProxy implements InvocationHandler {
+
+    /** 被代理的对象,真正的逻辑,还是要请求这个 */
+    private final Greet greet;
+
+    public GreetProxy(Greet greet) {
+        this.greet = greet;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("在真正调用之前");
+        greet.cheer();
+        System.out.println("在真正调用之后");
+        return null;
+    }
+}
