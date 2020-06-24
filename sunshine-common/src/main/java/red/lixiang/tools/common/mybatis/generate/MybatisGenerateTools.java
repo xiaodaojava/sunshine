9991fdb9a424bde15746f0d3b4ae0e133a5877cf
lixiang <lixiang9409@vip.qq.com>
2020-06-24 17:51:57 +0800

    jdk11用的方法

--- sunshine-common/src/main/java/red/lixiang/tools/common/mybatis/generate/MybatisGenerateTools.java
+++ sunshine-common/src/main/java/red/lixiang/tools/common/mybatis/generate/MybatisGenerateTools.java
@@ -30,35 +30,34 @@ public class MybatisGenerateTools {
 
 
 
-//    public static DomainDesc getDomainDescFromTable(SqlTable tableInfo,String packageName){
-//        //初始化thymeleaf
-//        TemplateEngine engine = TplConfig.getTemplateEngine();
-//        final Context ctx = new Context(Locale.CHINA);
-//        Map<String, Object> map = new HashMap<>(1);
-//        map.put("table",tableInfo);
-//        ctx.setVariables(map);
-//        ctx.setVariable("basePackage",packageName);
-//        // 默认的领域的名称就是表的名字全小写
-//        ctx.setVariable("domainName",tableInfo.getJavaTableName().toLowerCase());
-//        String dbModel = engine.process(DB_MODEL, ctx);
-//        String qc = engine.process(QC, ctx);
-//        String javaMapper = engine.process(JAVA_MAPPER, ctx);
-//        String xmlMapper = engine.process(XML_MAPPER, ctx);
-//        String provider = engine.process(PROVIDER, ctx);
-//        String manager = engine.process(MANAGER, ctx);
-//        String managerImpl = engine.process(MANAGER_IMPL, ctx);
-//        String controller = engine.process(CONTROLLER, ctx);
-//        DomainDesc domainDesc = new DomainDesc();
-//        domainDesc.setDbModel(dbModel)
-//                .setModelName(tableInfo.getJavaTableName())
-//                .setQc(qc)
-//                .setController(controller)
-//                .setManager(manager)
-//                .setManagerImpl(managerImpl)
-//                .setProvider(provider)
+    public static DomainDesc getDomainDescFromTable(SqlTable tableInfo,String packageName){
+        //初始化thymeleaf
+        TemplateEngine engine = TplConfig.getTemplateEngine();
+        final Context ctx = new Context(Locale.CHINA);
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("table",tableInfo);
+        ctx.setVariables(map);
+        ctx.setVariable("basePackage",packageName);
+        // 默认的领域的名称就是表的名字全小写
+        ctx.setVariable("domainName",tableInfo.getJavaTableName().toLowerCase());
+        String model = engine.process("text/mybatis/mysql-model.txt", ctx);
+        String qc = engine.process("text/mybatis/mysql-qc.txt", ctx);
+        String dao = engine.process("text/mybatis/mysql-dao-java.txt", ctx);
+        String provider = engine.process("text/mybatis/mysql-provider.txt", ctx);
+        String manager = engine.process("text/mybatis/mysql-manager-java.txt", ctx);
+        String managerImpl = engine.process("text/mybatis/mysql-manager-impl-java.txt", ctx);
+        String controller = engine.process("text/mybatis/mysql-controller-java.txt", ctx);
+        DomainDesc domainDesc = new DomainDesc();
+        domainDesc.setDbModel(model)
+                .setModelName(tableInfo.getJavaTableName())
+                .setQc(qc)
+                .setController(controller)
+                .setManager(manager)
+                .setManagerImpl(managerImpl)
+                .setProvider(provider)
 //                .setXmlMapper(xmlMapper)
-//                .setJavaMapper(javaMapper);
-//        return domainDesc;
-//    }
+                .setJavaMapper(dao);
+        return domainDesc;
+    }
 
 }
