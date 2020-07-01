c30f3294c44f628264013c388732cefef29bf65d
lixiang <lixiang9409@vip.qq.com>
2020-07-01 16:56:41 +0800

    jdk11: 处理器加了返回值

--- sunshine-starter/src/main/java/red/lixiang/tools/spring/processor/SimpleProcessor.java
+++ sunshine-starter/src/main/java/red/lixiang/tools/spring/processor/SimpleProcessor.java
@@ -13,8 +13,8 @@ public interface SimpleProcessor {
      * 在保存之前的操作
      * @param target
      */
-    default  void beforeSimpleSave(Object target){
-
+    default  Object beforeSimpleSave(Object target){
+        return target;
     }
 
     /**
@@ -30,8 +30,41 @@ public interface SimpleProcessor {
      * 在findByQuery之前的操作
      * @param query
      */
-    default void beforeSimpleQuery(BaseQC query){
+    default Object beforeSimpleQuery(BaseQC query){
+        return query;
+    }
 
+    /**
+     * 在findByQuery之后的操作
+     * @param object
+     * @return
+     */
+    default Object afterSimpleQuery(Object object){
+        return object;
+    }
+
+    default Object beforeSimpleGet(Object object){
+        return object;
+    }
+
+    default Object afterSimpleGet(Object object){
+        return object;
+    }
+
+    default Object beforeSimpleRemove(Object obj){
+        return obj;
+    }
+
+    default Object afterSimpleRemove(Object obj){
+        return obj;
+    }
+
+    default Object beforeSimpleRemoveByQuery(Object obj){
+        return obj;
+    }
+
+    default Object afterSimpleRemoveByQuery(Object obj){
+        return obj;
     }
 
 
