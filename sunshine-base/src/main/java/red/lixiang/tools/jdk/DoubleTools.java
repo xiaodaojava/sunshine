5e13632f2c1cb2b9216515cb9e44bfebb40d82a5
lixiang <lixiang9409@vip.qq.com>
2020-09-12 10:54:47 +0800

    加了一个工具类

--- sunshine-base/src/main/java/red/lixiang/tools/jdk/DoubleTools.java
+++ sunshine-base/src/main/java/red/lixiang/tools/jdk/DoubleTools.java
@@ -1,6 +1,7 @@
 package red.lixiang.tools.jdk;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @author lixiang
@@ -32,5 +33,52 @@ public class DoubleTools {
         return bd1.subtract(bd2).doubleValue();
     }
 
+    /**
+     * 提供精确的乘法运算。
+     *
+     * @param v1
+     *            被乘数
+     * @param v2
+     *            乘数
+     * @return 两个参数的积
+     */
+    public static double mul(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的小数位四舍五入处理。
+     *
+     * @param v
+     *            需要四舍五入的数字
+     * @param scale
+     *            小数点后保留几位
+     * @return 四舍五入后的结果
+     */
+    public static double round(double v, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                    "The scale must be a positive integer or zero");
+        }
+        BigDecimal b = new BigDecimal(Double.toString(v));
+        BigDecimal one = new BigDecimal("1");
+        return b.divide(one, scale, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    public static double div(double v1, double v2) {
+        return div(v1, v2, 10);
+    }
+
+    public static double div(double v1, double v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException(
+                    "The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
+    }
 
 }
