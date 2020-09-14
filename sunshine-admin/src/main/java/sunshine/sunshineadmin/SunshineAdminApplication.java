7a5b005ab9a28a4ba2859f20f31b786b3d8a1e94
lixiang <lixiang>
2020-09-14 20:32:51 +0800

    加了一个模块

---
+++ sunshine-admin/src/main/java/sunshine/sunshineadmin/SunshineAdminApplication.java
@@ -0,0 +1,13 @@
+package sunshine.sunshineadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunshineAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunshineAdminApplication.class, args);
+	}
+
+}
---
+++ sunshine-admin/src/test/java/sunshine/sunshineadmin/SunshineAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package sunshine.sunshineadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunshineAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
