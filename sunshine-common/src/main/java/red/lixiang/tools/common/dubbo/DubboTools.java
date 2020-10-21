e6359dcae076753222c2afff71d1b0a0453c8025
lixiang <lixiang1-jk@360shuke.com>
2020-10-21 21:58:31 +0800

    加了一个dubbo请求的类

--- sunshine-common/src/main/java/red/lixiang/tools/common/dubbo/DubboTools.java
+++ sunshine-common/src/main/java/red/lixiang/tools/common/dubbo/DubboTools.java
@@ -13,15 +13,36 @@ import java.util.Map;
  **/
 public class DubboTools {
 
-    public static void main(String[] args) {
-        DubboInvokeConfig param = new DubboInvokeConfig();
-        ReferenceConfig<GenericService> referenceConfig = ReferenceFactory.buildReferenceConfig(param);
+
+    public static void invokeNoArg(String url,String interfaceName, String methodName){
+        DubboInvokeConfig config = new DubboInvokeConfig();
+        config.setRegistryAddress(url);
+        config.setInterfaceName(interfaceName);
+        config.setMethodName(methodName);
+        doInvoke(config);
+    }
+
+    public static void invokeWithArgs(String url,String interfaceName, String methodName,String[] types,Object[] objs){
+        DubboInvokeConfig config = new DubboInvokeConfig();
+        config.setRegistryAddress(url);
+        config.setInterfaceName(interfaceName);
+        config.setMethodName(methodName);
+        config.setArgTypes(types);
+        config.setArgObjects(objs);
+        doInvoke(config);
+    }
+
+
+
+    public static void doInvoke(DubboInvokeConfig config) {
+        // 这个对象后面要缓存起来
+        ReferenceConfig<GenericService> referenceConfig = ReferenceFactory.buildReferenceConfig(config);
         GenericService genericService = referenceConfig.get();
-        Map<String, String> attachments = param.getAttachments();
+        Map<String, String> attachments = config.getAttachments();
         if (attachments != null) {
             RpcContext.getContext().setAttachments(attachments);
         }
-        Object result = genericService.$invoke(param.getMethodName(), param.getArgTypes(), param.getArgObjects());
+        Object result = genericService.$invoke(config.getMethodName(), config.getArgTypes(), config.getArgObjects());
         System.out.println(result);
     }
 }
