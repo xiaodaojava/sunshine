4dffe47211028fa5dd98c5ca59605ab06be0eb09
lixiang <lixiang9409@vip.qq.com>
2021-02-14 15:13:12 +0800

    insert的语句屏蔽关键字

--- sunshine-common/src/main/java/red/lixiang/tools/common/mybatis/MapperTools.java
+++ sunshine-common/src/main/java/red/lixiang/tools/common/mybatis/MapperTools.java
@@ -229,7 +229,7 @@ public class MapperTools {
                 }
 
                 // 如果值不为空的话，往里面拼接insertSQL
-                sql.VALUES(StringTools.camel2UnderScope(fieldName), String.format("#{%s}", fieldName));
+                sql.VALUES('`'+StringTools.camel2UnderScope(fieldName)+'`', String.format("#{%s}", fieldName));
             }
         } catch (Exception e) {
             logger.error("执行查询sql有错", e);
